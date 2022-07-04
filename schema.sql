b5a36252d2f5ffed7f8a687afcd29a30206d85fd
Tadesse-Alemayehu <email24t@gmail.com>
2022-07-04 21:18:04 +0300

    Create: database and table structure

--- schema.sql
+++ schema.sql
@@ -1,5 +1,14 @@
 /* Database schema to keep the structure of entire database. */
+DROP DATABASE if exists vet_clinic;
+CREATE DATABASE vet_clinic;
 
-CREATE TABLE animals (
-    name varchar(100)
-);
+CREATE TABLE IF NOT EXISTS animals (
+    id INT,
+    name varchar,
+    date_of_birth DATE,
+    escape_attempts INT,
+    neutered BOOLEAN,
+    weight_kg DECIMAL
+    );
+
+    
