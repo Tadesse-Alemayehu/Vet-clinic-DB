e290471fa5bc8166d584af2964705efcadce7086
Tadesse-Alemayehu <email24t@gmail.com>
2022-07-05 21:46:04 +0300

    Write all transaction queries

--- queries.sql
+++ queries.sql
@@ -8,3 +8,29 @@ select name, escape_attempts from animals where weight_kg>10.5;
 select * from animals where neutered=true;
 select * from animals where name not like 'Gabumon';
 select * from animals where weight_kg BETWEEN 10.4 and 17.3;
+
+BEGIN TRANSACTION;
+update animals set species='unspecified';
+select * from animals;
+ROLLBACK TRANSACTION;
+select * from animals;
+
+BEGIN TRANSACTION;
+update animals set species='digimon' where name like '%mon';
+update animals set species='pokemon' where species='';
+select * from animals;
+COMMIT TRANSACTION;
+select * from animals;
+
+BEGIN TRANSACTION;
+delete from animals; 
+ROLLBACK;
+select * from animals;
+
+Begin TRANSACTION;
+delete from animals where date_of_birth = '2022/1/1';
+SAVEPOINT SAVEPOINT_NAME;
+update animal set weight_kg=weight_kg*-1;
+ROLLBACK TO SAVEPOINT_NAME;
+update animals set weight_kg=weight_kg*-1 where weight_kg<0;
+ROLLBACK TRANSACTION;
