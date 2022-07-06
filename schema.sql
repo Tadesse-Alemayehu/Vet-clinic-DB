f0acb3d0048c72e96cec3051a31311c3e2f47ba9
Tadesse-Alemayehu <email24t@gmail.com>
2022-07-06 20:14:27 +0300

    UPDATE_CREATE: schema structures

--- schema.sql
+++ schema.sql
@@ -11,17 +11,39 @@ CREATE TABLE IF NOT EXISTS animals (
 );
 ALTER TABLE animals
 ADD COLUMN species varchar;
+
+/* Vet clinic database: query multiple tables */
 CREATE TABLE owners(
     id INT GENERATED ALWAYS AS IDENTITY,
     full_name varchar,
     age INT,
     PRIMARY KEY(id)
 );
+
 CREATE TABLE species(
-    id INT GENERATED ALWAYS AS IDENTITY,
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name varchar
 );
+
+ALTER TABLE animals ALTER COLUMN id 
+SET NOT NULL;
+
+ALTER TABLE animals ALTER COLUMN id 
+ADD GENERATED ALWAYS AS IDENTITY;
 
 ALTER TABLE animals 
 ADD CONSTRAINT animal_primary_key 
 PRIMARY KEY(id);
+
+ALTER TABLE animals DROP COLUMN species;
+
+ALTER TABLE animals ADD COLUMN species_id INT;
+
+ALTER TABLE animals ADD CONSTRAINT species_foregn_key 
+FOREIGN KEY(species_id) REFERENCES species(id);
+
+ALTER TABLE animals ADD COLUMN owner_id INT;
+
+ALTER TABLE animals ADD CONSTRAINT owner_forign_key 
+FOREIGN KEY(owner_id) REFERENCES owners(id);
+
