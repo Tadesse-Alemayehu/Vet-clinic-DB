fe983c7c2ee2130450aa8d3dc834f2ad4d5286fd
Tadesse Alemayehu <tadalem26@gmail.com>
2022-07-06 16:45:15 +0300

    Create: species table

--- schema.sql
+++ schema.sql
@@ -1,7 +1,6 @@
 /* Database schema to keep the structure of entire database. */
 DROP DATABASE if exists vet_clinic;
 CREATE DATABASE vet_clinic;
-
 CREATE TABLE IF NOT EXISTS animals (
     id INT,
     name varchar,
@@ -9,14 +8,16 @@ CREATE TABLE IF NOT EXISTS animals (
     escape_attempts INT,
     neutered BOOLEAN,
     weight_kg DECIMAL
-    );
-
-ALTER TABLE animals ADD COLUMN species varchar;
-    
+);
+ALTER TABLE animals
+ADD COLUMN species varchar;
 CREATE TABLE owners(
     id INT GENERATED ALWAYS AS IDENTITY,
     full_name varchar,
     age INT,
     PRIMARY KEY(id)
-    );
-    
+);
+CREATE TABLE species(
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name varchar
+);
