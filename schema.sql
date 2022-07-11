392c9568c42905236b8418c243896af14c1e5482
Tadesse-Alemayehu <email24t@gmail.com>
2022-07-11 20:26:10 +0300

    Update: schema structure

--- schema.sql
+++ schema.sql
@@ -1,81 +1,50 @@
 /* Database schema to keep the structure of entire database. */
 DROP DATABASE if exists vet_clinic;
 CREATE DATABASE vet_clinic;
-CREATE TABLE IF NOT EXISTS animals (
-    id INT,
-    name varchar,
-    date_of_birth DATE,
-    escape_attempts INT,
-    neutered BOOLEAN,
-    weight_kg DECIMAL
-);
-ALTER TABLE animals
-ADD COLUMN species varchar;
-
-/* Vet clinic database: query multiple tables */
 CREATE TABLE owners(
-    id INT GENERATED ALWAYS AS IDENTITY,
-    full_name varchar,
-    age INT,
-    PRIMARY KEY(id)
+  id INT GENERATED ALWAYS AS IDENTITY,
+  full_name VARCHAR(20),
+  age INT,
+  PRIMARY KEY(id)
 );
 
 CREATE TABLE species(
-    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    name varchar
+  id INT GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(20),
+  PRIMARY KEY(id)
 );
 
-ALTER TABLE animals ALTER COLUMN id 
-SET NOT NULL;
+CREATE TABLE animals(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(20),
+  date_of_birth DATE,
+  escape_attempts INT,
+  neutered BOOLEAN,
+  weight_kg DECIMAL,
+  species_id INT REFERENCES species(id),
+  owner_id INT REFERENCES owners(id),
+  PRIMARY KEY(id)
+);
 
-ALTER TABLE animals ALTER COLUMN id 
-ADD GENERATED ALWAYS AS IDENTITY;
-
-ALTER TABLE animals 
-ADD CONSTRAINT animal_primary_key 
-PRIMARY KEY(id);
-
-ALTER TABLE animals DROP COLUMN species;
-
-ALTER TABLE animals ADD COLUMN species_id INT;
-
-ALTER TABLE animals ADD CONSTRAINT species_foregn_key 
-FOREIGN KEY(species_id) REFERENCES species(id);
-
-ALTER TABLE animals ADD COLUMN owner_id INT;
-
-ALTER TABLE animals ADD CONSTRAINT owner_forign_key 
-FOREIGN KEY(owner_id) REFERENCES owners(id);
-
-/* Vet clinic database: add "join table" for visits */
-
-/* Create a table named vets */
 CREATE TABLE vets(
-    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, 
-    name varchar,
-    age INT,
-    date_of_graduation DATE
+  id INT GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(20),
+  age INT,
+  date_of_graduation DATE,
+  PRIMARY KEY(id)
 );
 
-/* many-to-many relationship between the tables species and vets */
-DROP table if EXISTS specialties;
-CREATE TABLE specialties(
-    vet INT,
-    species INT,
-    CONSTRAINT vet_specializes_in
-    FOREIGN KEY(vet) REFERENCES vets(id),
-    CONSTRAINT species_specialized_vets
-    FOREIGN KEY(species) REFERENCES species(id)
+CREATE TABLE specializations(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  species_id INT REFERENCES species(id),
+  vet_id INT REFERENCES vets(id),
+  PRIMARY KEY(id)
 );
 
-/* many-to-many relationship between the tables animals and vets */
-DROP table if EXISTS visits;
 CREATE TABLE visits(
-    animal INT,
-    vet INT,
-    visit_date date,
-    CONSTRAINT animal_visit_FK
-    FOREIGN KEY(animal) REFERENCES animals(id),
-    CONSTRAINT vet_visited_FK 
-    FOREIGN KEY(vet) REFERENCES vets(id)
+  id INT GENERATED ALWAYS AS IDENTITY,
+  animal_id INT REFERENCES animals(id),
+  vet_id INT REFERENCES vets(id),
+  date_of_visit DATE,
+  PRIMARY KEY(id)
 );
